531e05ea2a64cf9af8e6a8ae204d32c8a013ea28
Shaun <shaunv.1000@gmail.com>
2024-12-06 19:03:26 +1030

    Some rotation working

--- TeamCode/src/org/firstinspires/ftc/teamcode/shared/opmodes/AutonomousMechanum.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/shared/opmodes/AutonomousMechanum.java
@@ -30,10 +30,10 @@ public class AutonomousMechanum extends RobotOpMode {
                 config.backLeftMotor, config.backRightMotor, true);
 
 //        tasks.add(new MecanumDriveTask(this, 1, drive, 1, 0, 0));
-        tasks.add(new GoToTask(this, 1, drive, config.otos, 250, 0, 0.3, 20));
-        tasks.add(new GoToTask(this, 1, drive, config.otos, 250, 250, 0.3, 20));
-        tasks.add(new GoToTask(this, 1, drive, config.otos, 0, 250, 0.3, 20));
-        tasks.add(new GoToTask(this, 1, drive, config.otos, 0, 0, 0.3, 20));
+        tasks.add(new GoToTask(this, 4, drive, config.otos, 0, 500, 0, 0.3, 20, 1));
+        tasks.add(new GoToTask(this, 4, drive, config.otos, 500, 500, 90,0.3, 20, 20));
+        tasks.add(new GoToTask(this, 4, drive, config.otos, 500, 0, 180, 0.3, 20, 20));
+        tasks.add(new GoToTask(this, 4, drive, config.otos, 0, 0, 270, 0.3, 20, 20));
 //        tasks.add(new MecanumDriveTask(this, 1, drive, 0, 1, 0));
 //        tasks.add(new MecanumDriveTask(this, 3, drive, 0, 0.1, 1));
 
--- TeamCode/src/org/firstinspires/ftc/teamcode/shared/tasks/GoToTask.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/shared/tasks/GoToTask.java
@@ -12,20 +12,27 @@ public class GoToTask extends BaseTask  implements Task {
     private final double target_x;
     private final double target_y;
 
+    private final double target_h;
+
     private final double max_speed;
-    private final double target_error;
+    private final double distance_error;
+    private final double angle_error;
     private SparkFunOTOS.Pose2D current;
     private double distance;
+    private double angle_distance;
 
-    public GoToTask(RobotOpMode opMode, double time, NormalisedMecanumDrive drive, SparkFunOTOS odometry, double x, double y, double speed, double error) {
+    public GoToTask(RobotOpMode opMode, double time, NormalisedMecanumDrive drive, SparkFunOTOS odometry,
+                    double x, double y, double h, double speed, double distance_error, double angle_error) {
         super(opMode, time);
 
         this.drive = drive;
         this.odometry = odometry;
         this.target_x = x;
         this.target_y = y;
+        this.target_h = h;
         this.max_speed = speed;
-        this.target_error = error;
+        this.distance_error = distance_error;
+        this.angle_error = angle_error;
     }
 
     @Override
@@ -34,6 +41,7 @@ public class GoToTask extends BaseTask  implements Task {
         double deltax = target_x - current.x;
         double deltay = target_y - current.y;
         distance = Math.sqrt(deltax*deltax+deltay*deltay);
+        angle_distance = target_h - current.h;
         opMode.telemetry.addLine("distance " + distance);
     }
 
@@ -46,6 +54,7 @@ public class GoToTask extends BaseTask  implements Task {
         double deltay = target_y - current.y;
         distance = Math.sqrt(deltax*deltax+deltay*deltay);
         double angle = Math.atan2(deltay, deltax);
+        angle_distance = target_h - current.h;
         double speed = max_speed;
         if (distance < 300) {
             speed = distance/300;
@@ -53,7 +62,7 @@ public class GoToTask extends BaseTask  implements Task {
                 speed = 0.1;
             }
         }
-        System.out.println(String.format("dX %4.2f  dY %4.2f  d %4.2f h %4.1f", deltax, deltay, distance, angle*180.0/Math.PI));
+        System.out.println(String.format("dX %4.2f  dY %4.2f  d %4.2f dh %4.1f h %4.1f heading %4.1f", deltax, deltay, distance, angle_distance, current.h, angle*180.0/Math.PI));
         opMode.telemetry.addLine("Distance: " + distance);
         opMode.telemetry.addLine("Angle: " + angle);
         opMode.telemetry.addLine("Speed: " + speed);
@@ -62,13 +71,19 @@ public class GoToTask extends BaseTask  implements Task {
             return;
         }
 
-        drive.setSpeedXYR(deltax/distance * speed, -deltay/distance * speed, 0);
+        double max_delta_pos = Math.max(deltax, deltay);
+        double speedx = (max_delta_pos > 0.0) ? deltax / max_delta_pos * max_speed : 0.0;
+        double speedy = (max_delta_pos > 0.0) ? deltay / max_delta_pos * max_speed : 0.0;
+        double speedr = Math.min(angle_distance / 5, max_speed);
+
+
+        drive.setSpeedXYR(speedx, -speedy, speedr);
 //        drive.setSpeedXYR(deltax/distance, 0, 0);
         drive.update();
     }
 
     @Override
     public boolean isFinished() {
-        return super.isFinished() || distance < target_error;
+        return super.isFinished() || (distance < distance_error && angle_distance < angle_error);
     }
 }
